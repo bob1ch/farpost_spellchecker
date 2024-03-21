521e921de3b94a28629da8f0ffee0847298ab8a1
bob1ch <bobich1488@mail.ru>
2024-03-21 12:08:17 +1000

    other changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-
+from my_spellchecker import my_sp
 
 
 
@@ -8,7 +8,11 @@ import argparse
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-t', '--test', required = False, help = 'test help correct', action = argparse.BooleanOptionalAction)
+    ap.add_argument('-msp', '--my_spellchecker', required = False, help = 'my implementation of spellchecker', action = argparse.BooleanOptionalAction)
     args = vars(ap.parse_args())
 
     if args['test']:
         print('test message')
+
+    if args['my_spellchecker']:
+        my_sp.get_words()
